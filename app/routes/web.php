f63b2963a60c9d6a65ca5217489cb9d39e6d7974
JakubKermes <jakub.kermes@studenci.collegiumwitelona.pl>
2023-10-07 14:11:32 +0200

    added laravel/ui

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/points/{budget}/{handling}/{speed}/{comfort}/{looks}', function ($budget, $handling, $speed, $comfort, $looks) {
+
+});
+
+
